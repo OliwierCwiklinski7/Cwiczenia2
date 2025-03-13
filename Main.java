198e8963e758eb11d01020fde42b003d981b5236
s33360 <s33360@pjwstk.edu.pl>
2025-03-13 11:03:54 +0100

    Zadanie 1

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[] array = {7,8,9,10,11};
+        int[] filteredArray = filterEvenIndexOddValue(array);
+        System.out.println(Arrays.toString(filteredArray));
     }
+    public static int[] filterEvenIndexOddValue(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 != 0) {
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 != 0) {
+                result[index++] = array[i];
+            }
+        }
+        return result;
+    }
+
 }
