c58bc354cceaa5e79c983d1914259818f4949eb7
s33360 <s33360@pjwstk.edu.pl>
2025-03-18 11:27:12 +0100

    Zadanie 3

--- Main.java
+++ Main.java
@@ -1,23 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {3,3,3,2,2};
-        System.out.println(findDominant(array));
+        int[] array = {1, 2, 3};
+        int position = 3;
+        System.out.println(Arrays.toString(rotateArray(array, position)));
 
     }
+    public static int[] rotateArray(int[] array, int positions) {
+        int x = array.length;
+        positions = positions % x;
+        int[] wynik = new int[x];
+        for (int i = 0; i < x; i++) {
+            wynik[(i + positions) %  x] = array[i];
 
-    public static int findDominant(int[] array) {
-        int z = array.length;
-            for (int i = 0; i < z; i++) {
-                int count = 0;
-
-            for (int j = 0; j < z; j++) {
-                if (array[i] == array[j]) {
-                count++;
-            }
         }
-            if (count > z/2) {
-            return array[i];
-        }
+        return wynik;
     }
-        return -1;
-    }}
+    }
